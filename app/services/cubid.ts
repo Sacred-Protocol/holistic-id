ec9f9d8cd4fa54b538edfa484750d5ba00cee2a2
skyfoxx <skyfoxx@sacred.finance>
2024-10-06 08:32:35 +1100

    feat: fetch user details

--- app/services/cubid.ts
+++ app/services/cubid.ts
@@ -23,6 +23,24 @@ interface CreateUserResponse {
     error: string | null;
 }
 
+interface UserData {
+    name: string;
+    address: string;
+    country: string;
+    coordinates: {
+        lat: number;
+        lon: number;
+    };
+    is_human: string;
+    error: string | null;
+}
+
+interface UserScore {
+    cubid_score: number;
+    scoring_schema: number;
+    error: string | null;
+}
+
 export const fetchIdentity = async (
     uuid: string
 ): Promise<FetchIdentityResponse> => {
@@ -58,6 +76,56 @@ export const createUser = async (
         return response.data;
     } catch (error) {
         console.error('Error creating user:', error);
+        throw error;
+    }
+};
+
+export const fetchUserData = async (userId: string): Promise<UserData> => {
+    try {
+        const response = await axios.post<UserData>(
+            `${API_BASE_URL}/v2/identity/fetch_user_data`,
+            {
+                apikey: process.env.NEXT_PUBLIC_CUBID_API_KEY,
+                user_id: userId,
+            }
+        );
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching user data:', error);
+        throw error;
+    }
+};
+
+export const fetchUserScore = async (userId: string): Promise<UserScore> => {
+    try {
+        const response = await axios.post<UserScore>(
+            `${API_BASE_URL}/v2/score/fetch_score`,
+            {
+                apikey: process.env.NEXT_PUBLIC_CUBID_API_KEY,
+                user_id: userId,
+            }
+        );
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching user score:', error);
+        throw error;
+    }
+};
+
+export const fetchUserIdentity = async (
+    userId: string
+): Promise<FetchIdentityResponse> => {
+    try {
+        const response = await axios.post<FetchIdentityResponse>(
+            `${API_BASE_URL}/v2/identity/fetch_identity`,
+            {
+                apikey: process.env.NEXT_PUBLIC_CUBID_API_KEY,
+                user_id: userId,
+            }
+        );
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching user identity:', error);
         throw error;
     }
 };
